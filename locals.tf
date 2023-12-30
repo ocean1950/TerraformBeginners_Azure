2583d4ac5aa8f5fa2f3c95b538d93d748be8d27d
Cloud Scholars <154997540+CloudScholars@users.noreply.github.com>
2023-12-29 23:37:48 -0500

    Delete locals.tf

--- locals.tf
+++
@@ -1,9 +0,0 @@
-locals {
-  resource_group_name   = "mkt-rsg"
-  location              = "East US"
-  networksecuritygrp    = "mkt-netsec"
-  virtual_network ={
-    name  = "mkt-virtualnetwork"
-    address_space = "11.0.0.0/16"
-  }
-}
