2ee5f314c2df7d9b1db5b0e718131348573cd900
Cloud Scholars <154997540+CloudScholars@users.noreply.github.com>
2024-01-03 16:45:51 -0500

    Update main.tf

--- Creating a virtual machine (public IP) Part 2 - Finished Code/main.tf
+++ Creating a virtual machine (public IP) Part 2 - Finished Code/main.tf
@@ -8,10 +8,10 @@ terraform {
 }
 
 provider "azurerm" {
-  subscription_id = "990ddbdf-bcea-4f83-beb2-7c36343c8770"
-  tenant_id = "576feb8d-023e-4ab0-97f5-c3a3dd062e20"
-  client_id = "a449e5d0-f284-401d-9d0d-c0b8a1b58e07"
-  client_secret = "AX18Q~Z4R5BxEknP_PY6CscY9OHZcy453FlZucgR"
+  subscription_id = "Your Subscription ID"
+  tenant_id = "Your Tenant ID"
+  client_id = "Your Client ID"
+  client_secret = "Your client Secret"
 
   features {
     
