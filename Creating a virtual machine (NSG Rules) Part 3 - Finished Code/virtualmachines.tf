317f293dc4321bfdf44c11c3497ae5de9bd370d5
Cloud Scholars <154997540+CloudScholars@users.noreply.github.com>
2024-01-05 10:03:24 -0500

    upload completed files

---
+++ Creating a virtual machine (NSG Rules) Part 3 - Finished Code/virtualmachines.tf
@@ -0,0 +1,28 @@
+resource "azurerm_windows_virtual_machine" "mkt-vm01" {
+  name                = "mkt-vm01"
+  resource_group_name = local.resource_group_name
+  location            = local.location
+  size                = "Standard_B4ms"
+  admin_username      = "adminuser"
+  admin_password      = "Scholars123$"
+  network_interface_ids = [
+    azurerm_network_interface.mkt-netinterface.id
+  ]
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "MicrosoftWindowsDesktop"
+    offer     = "windows-11"
+    sku       = "win11-21h2-avd"
+    version   = "latest"
+  }
+
+  depends_on = [ 
+    azurerm_network_interface.mkt-netinterface,
+    azurerm_resource_group.mkt-rsg
+    ]
+}
