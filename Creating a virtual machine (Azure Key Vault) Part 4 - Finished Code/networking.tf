b8577881afad2643661fd5e231d00b424a3d2aa0
Cloud Scholars <154997540+CloudScholars@users.noreply.github.com>
2024-01-08 16:01:55 -0500

    Update networking.tf

--- Creating a virtual machine (Azure Key Vault) Part 4 - Finished Code/networking.tf
+++ Creating a virtual machine (Azure Key Vault) Part 4 - Finished Code/networking.tf
@@ -11,7 +11,7 @@ resource "azurerm_network_security_group" "mkt-netsec" {
     protocol                   = "Tcp"
     source_port_range          = "*"
     destination_port_range     = "3389" 
-    source_address_prefixes    = ["81.168.172.211", "71.168.176.211"]
+    source_address_prefixes    = ["Your public ip here"]
 
     destination_address_prefix = "*"
   }
@@ -97,18 +97,3 @@ resource "azurerm_subnet_network_security_group_association" "subnet2associate" 
 
 
 
-/*
-
-    security_rule {
-    name                       = "RDP"
-    priority                   = 102
-    direction                  = "Inbound"
-    access                     = "Allow"
-    protocol                   = "Tcp"
-    source_port_range          = "*"
-    destination_port_range     = "3389"
-    source_address_prefix      = "71.168.176.211"
-    destination_address_prefix = "*"
-  }
-
-  */
