e4e30337274500866219e92eb7d5e1125d9150eb
Prem Prakash <42059137+ocean1950@users.noreply.github.com>
2024-08-29 18:54:41 +0530

    Adding the New RG

--- Virtual Network Terraform (Finished Code)/locals.tf
+++ Virtual Network Terraform (Finished Code)/locals.tf
@@ -7,3 +7,13 @@ locals {
     address_space = "11.0.0.0/16"
   }
 }
+
+locals {
+  resource_group_name   = "mkt-rsg_01"
+  location              = "West US"
+  networksecuritygrp    = "mkt-netsec_01"
+  virtual_network ={
+    name  = "mkt-virtualnetwork_01"
+    address_space = "12.0.0.0/16"
+  }
+}
